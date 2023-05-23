ce30c923ba1a07f6eb9e2e9a5e2bfc9297cdb445
swefart <swefart@gmail.com>
2023-05-23 16:52:30 +0200

    homework done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import {save, load} from "./storage"
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe)
+
+const storageKey = 'videoplayer-current-time';
+
+
+ player.on('timeupdate', throttle((data) => {
+        save(storageKey, data.seconds)
+ }, 1000));
+        
+document.addEventListener("DOMContentLoaded", setCurrentTimeAfterReload)
+
+function setCurrentTimeAfterReload() {
+    const currentTime = load(storageKey)
+    if (currentTime === undefined) {
+        player.setCurrentTime(0)
+    } 
+    player.setCurrentTime(currentTime)
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import {save, load, remove} from "./storage"
+import throttle from "lodash.throttle"
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+let data = load(STORAGE_KEY)
+
+
+const formEl = document.querySelector('form')
+
+formEl.addEventListener('input', throttle(foo, 500))
+document.addEventListener("DOMContentLoaded", setDataToForm())
+formEl.addEventListener('submit', submitForm)
+
+
+
+function foo(e) {  
+  formData[e.target.name] = e.target.value
+  save(STORAGE_KEY, formData)
+}
+
+function setDataToForm() {
+    if (!data) return 
+    formEl.elements.email.value = data.email;
+    formEl.elements.message.value = data.message;
+}
+
+function submitForm(e) {
+    e.preventDefault()
+    remove(STORAGE_KEY)
+    formEl.reset()
+    console.log(formData);
+}
 
